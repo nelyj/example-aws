1144c197b9bedbd17abe49ea986782ab24170962
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-22 15:00:42 -0300

    Agrego nginx restart service

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
+      run "#{sudo} service nginx #{command}"
     end
   end
 
