aa3f8f8b9570dc271fba4ce9c2784e92c6f450fa
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-22 17:18:33 -0300

    add nginx reload

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,10 @@ set :stage,           :production
 # set :pty, true
 
 # Default value for :linked_files is []
-#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
 #set :linked_files, %w{config/database.yml}
+
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for linked_dirs is []
@@ -40,7 +42,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-      run "#{sudo} service nginx #{command}"
+      run "#{sudo} nginx -s reload"
     end
   end
 
