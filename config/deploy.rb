a7c4b2d6a729596f72c6c6145ece6ccb00712477
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-22 22:26:17 -0300

    more changes test

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-      run "#{sudo} nginx -s reload"
     end
   end
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '54.88.185.110', user: 'deploy', roles: %w{app web}, my_property: :my_value
+server '54.165.220.156', user: 'deploy', roles: %w{app web}, my_property: :my_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
 
