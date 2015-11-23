08e22e90291109612bf5892d6e34bb740caf8e99
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-23 00:34:52 -0300

    Add sudo for passenger restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :deploy_to, '/home/deploy/example-aws'
 # Default value for :scm is :git
 # set :scm, :git
 set :stage,           :production
+set :passenger_restart_with_sudo, true
 # Default value for :format is :pretty
 # set :format, :pretty
 
