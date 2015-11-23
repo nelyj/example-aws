7eba9f3f1ee8d1162d7cf2ed46e8d70f2c42c108
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-23 00:36:42 -0300

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,6 @@ set :deploy_to, '/home/deploy/example-aws'
 # Default value for :scm is :git
 # set :scm, :git
 set :stage,           :production
-set :passenger_restart_with_sudo, true
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -20,7 +19,8 @@ set :passenger_restart_with_sudo, true
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
+set :passenger_restart_with_sudo, true
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
